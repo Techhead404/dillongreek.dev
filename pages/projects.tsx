2610b5e898deea0bcde3adbe4413e096fec53a61
Dillon Greek <techhead404@gmail.com>
2024-06-19 11:15:22 -0500

    Project page update

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -9,62 +9,56 @@ import healing from "../assets/healingpeace.jpg";
 
 const inter = Roboto_Mono({ subsets: ["latin"] });
 
-
 export default function Projects() {
-  const projects = [
-    {
-      image: stock,
-      alt: "Github Profile Picture",
-      link: "https://github.com/techhead404",
-      title: "GitHub",
-      description: "My GitHub."
-    },
-    {
-      image: bubble,
-      alt: "Bubble Danger Icon",
-      link: "https://github.com/Techhead404/BubbleDanger",
-      title: "Bubble Danger",
-      description: "Android game built with Unity."
-    },
-    {
-      image: tech,
-      alt: "Techs Site Screen Shot",
-      link: "http://techssite.com",
-      title: "Techs Site",
-      description: "My blog for sharing project and reviews."
-    },
-    {
-      image: healing,
-      alt: "Healing Peace Screen Shot",
-      link: "http://HealingPeaceMassage.com",
-      title: "Healing Peace Massage",
-      description: "Website for a local massage business."
-    }
-  ];
-
   return (
     <main className="flex min-h-screen flex-col pt-10">
-      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-7 w-fill h-auto md:w-4/5 lg:w-full mx-auto">
-        {projects.map((project, index) => (
-          <div className="w-auto h-1/2 text-center" key={index}>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-7 p-20 w-fill md:w-4/5 mx-auto">
+        <div className="bg-white rounded-lg shadow-lg p-5 w-auto">
             <Image
-              src={project.image}
-              alt={project.alt}
-              style={{ width: "auto", height: "100%", borderRadius: "15%", margin: "auto", objectFit: "fill"}}
-            />
-            <h4 className="text-xl text-sky-400 font-bold mt-2">
-              {project.link ? (
-                <a href={project.link} target="_blank" className="text-sky-400 text-pretty">
-                  {project.title}
-                </a>
-              ) : (
-                project.title
-              )}
-            </h4>
-            <p className="text-gray-500">{project.description}</p>
+              src={stock}
+              alt="Github Profile Picture"
+              style={
+                { width: "fill -content", height:"auto", margin: "auto"}}
+              >
+            </Image>
+            <h4 className="text-xl text-sky-400 font-bold mt-2"><a href="https://github.com/techhead404" target="_blank" className="text-sky-400 text-pretty">GitHub</a></h4>
+            <p className="text-gray-500">My GitHub.</p>
           </div>
-        ))}
-      </div>
+          <div className="bg-white rounded-lg shadow-lg p-5 w-auto">
+            <Image
+              src={bubble}
+              alt="Bubble`Danger Icon"
+              style={
+                { width: "fill -content", height:"auto", margin: "auto"}}
+              >
+            </Image>
+            <h4 className="text-xl text-sky-400 font-bold mt-2">Bubble Danger</h4>
+            <p className="text-gray-500">Android game built with Unity.</p>
+          </div>
+          <div className="bg-white rounded-lg shadow-lg p-5 w-auto">
+            <Image
+              src={tech}
+              alt="Techs Site Screen Shot"
+              style={
+                { width: "fill -content", height:"auto", margin: "auto"}}
+              >
+            </Image>
+            <h4 className="text-xl text-sky-400 font-bold mt-2"><a href="http://techssite.com" target="_blank" className="text-sky-400 text-pretty">Techs Site</a></h4>
+            <p className="text-gray-500">My blog for sharing project and reviews.</p>
+          </div>
+          <div className="bg-white rounded-lg shadow-lg p-5 w-auto">
+            <Image
+              src={healing}
+              alt="Healing Peace Screen Shot"
+              style={
+                { width: "fill -content", height:"auto", margin: "auto"}}
+              >
+            </Image>
+            <h4 className="text-xl text-sky-400 font-bold mt-2"><a href="http://HealingPeaceMassage.com" target="_blank" className="text-sky-400 text-pretty">Healing Peace Massage</a></h4>
+            <p className="text-gray-500">Website for a local massage business.</p>
+          </div>
+        </div>
     </main>
   );
 }
